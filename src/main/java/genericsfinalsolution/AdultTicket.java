3202fb262cef1e5aa599afe08837c6ef4ed1dc28
Pedram Ezzati <pedram.ezzati@ceitasl.com>
2023-11-05 10:38:13 +0330

    generics final solution

---
+++ src/main/java/genericsfinalsolution/AdultTicket.java
@@ -0,0 +1,15 @@
+package genericsfinalsolution;
+
+public class AdultTicket extends Ticket {
+
+    @Override
+    public int getPrice() {
+        return 10;
+    }
+
+    @Override
+    public String toString() {
+        return "Adult ticket";
+    }
+
+}
---
+++ src/main/java/genericsfinalsolution/Ticket.java
@@ -0,0 +1,6 @@
+package genericsfinalsolution;
+
+public abstract class Ticket {
+    public abstract int getPrice();
+
+}
